7ae2da0986b267e87c0cbe1869af8a0fd74695f1
Gokul Kishore T <kishorethiyagarajan7@gmail.com>
2025-08-02 19:48:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,19 +31,19 @@ def submit():
     k=md.predict(arr)
     pr = k[0]
     assignp = assignment*100
-    client = client = InferenceClient(provider='novita',token=os.getenv("HF_APIKEY"))
+    clientx = InferenceClient(provider='novita',token=os.getenv("HF_APIKEY"))
     iparam = f'As a teacher you are speaking to a student. Generate a suggestion or feedback not more than 400 words for a student based on their CGPA {cgpa}, credits {credits}, extra-curriculars {extraCurricular}, projects {projects}, self-study hours {selfStudy}, interest in subjects {engagement} out of 10, assignment completion {assignp}, need of faculty contribution for the student is {contribution} out of 10, and improvement potential {pr} out of 10 without highlighting any numerical data in the response.'
     messages = [
 	{ "role": "user", "content": iparam }
     ]
-    stream = client.chat.completions.create(
+    streamx = clientx.chat.completions.create(
     model='Qwen/Qwen3-Coder-480B-A35B-Instruct', 
 	messages=messages, 
 	max_tokens=1000,
 	stream=True
     )
     p = ""
-    for chunk in stream:
+    for chunk in streamx:
         content = chunk.choices[0].delta.content
         if content is not None:
             p += content
