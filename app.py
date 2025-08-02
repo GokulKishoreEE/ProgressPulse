6724b2a7a9b73cbd7b49ebd0044b6cd877ece70f
Gokul Kishore T <kishorethiyagarajan7@gmail.com>
2025-08-02 19:05:27 +0530

    Update 2 app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from huggingface_hub import InferenceClient
 import os
 import webbrowser
 from dotenv import load_dotenv
+load_dotenv()
 with open('./trainmlx.pkl','rb') as f:
     md = pickle.load(f)
 app = Flask(__name__,template_folder='templates')
@@ -29,7 +30,7 @@ def submit():
     k=md.predict(arr)
     pr = k[0]
     assignp = assignment*100
-    client = client = InferenceClient(provider='novita',token=os.environ["HF_APIKEY"])
+    client = client = InferenceClient(provider='novita',token=os.getenv("HF_APIKEY"))
     iparam = f'As a teacher you are speaking to a student. Generate a suggestion or feedback not more than 400 words for a student based on their CGPA {cgpa}, credits {credits}, extra-curriculars {extraCurricular}, projects {projects}, self-study hours {selfStudy}, interest in subjects {engagement} out of 10, assignment completion {assignp}, need of faculty contribution for the student is {contribution} out of 10, and improvement potential {pr} out of 10 without highlighting any numerical data in the response.'
     messages = [
 	{ "role": "user", "content": iparam }
